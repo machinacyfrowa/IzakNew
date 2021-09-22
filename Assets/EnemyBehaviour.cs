4904742510bd6924cea7d111cc1963f1b0543e52
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2021-09-22 12:52:19 +0200

    enemy movement and hit

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public int hp = 3;
+    public float speed = 3;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        transform.position += direction * Time.deltaTime * speed;
+        //Debug.Log(direction.ToString());
+        //Vector2.Lerp(transform.position, player.transform.position, Time.deltaTime);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), Time.deltaTime);
+    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    hp--;
+    //    if(hp <= 0)
+    //        Destroy(transform.gameObject);
+    //}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.collider.gameObject);
+        hp--;
+        if (hp <= 0)
+            Destroy(transform.gameObject);
+    }
+}
